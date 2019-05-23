e941c6b7cdea98fd6b4411a31a2afec1596b712e
Leonardino80 <gilgamesh2005@gmail.com>
2019-05-23 03:25:06 +0200

    consegna!

--- main.js
+++ main.js
@@ -1,44 +1,53 @@
+// Utilizzare l’API: https://www.boolean.careers/api/array/basket?n=numberPlayers
+// Chiedere all’utente quanti giocatori vuole generare. Chiedere all’API i giocatori e stampare a schermo una card per ogni giocatore attraverso handlebars.
+//
+// Inizialmente chiamate l'API con un numero fisso stabilito da voi, in modo da interrogare l'API nel modo corretto e stilare le card. Poi aggiungete la richiesta del numero dei giocatori all'utente
+
+// dichiaro la variabile di quanti giocatori voglio generare
+var volte
+
+// CREO IL TEMPLATE CON Handlebars
+var source = $('#basket-Handlebars').html();
+var template = Handlebars.compile(source);
+
 $(document).ready(function(){
-
-  // CREO IL TEMPLATE CON Handlebars
-  var source = $('#basket-Handlebars').html();
-  var template = Handlebars.compile(source);
-
-
-
-
-  // appendo i quadratini nel container
-  // for (var i = 0; i < 36; i++) {
+  $('button').click( function () {
+    // alla variabile volte assegno il valore di input
+    volte = $('input').val()
+    // parte la funzione ajax con n = volte
     $.ajax({
       'url' : "https://www.boolean.careers/api/array/basket",
+      // argomenti del data per non scriverli sulla stringa
       'data' :  {
-        'n' : 1,
+        'n' : volte,
       },
       'success' : function (data) {
-      // FISSO I CAMPI COMPILABILI DI HANDLEBARS
-      var context = {
-        // IL TEMPLATE GENERA LO STESSO NUMERO RANDOM 36 VOLTE
-        'playerCode' : data.response[0].playerCode ,
-        'rebounds' : data.response[0].rebounds ,
-        'fouls' : data.response[0].fouls ,
-        'points' : data.response[0].points ,
-        'twoPoints' : data.response[0].twoPoints ,
-        'threePoints' : data.response[0].threePoints ,
-        'number' : 7
-      };
-      var html = template(context);
-
-      $('.container').append(html);
-
-
-      console.log(data.response[0]);
-
-
-
+        for (var i = 0; i < volte; i++)
+        {
+          // dentro success (che avviene n VOLTE) compilo la scheda giocatore
+          var context = {
+            // parametri ottenuti con console.log del data.response
+            'playerCode' : data.response[i].playerCode ,
+            'rebounds' : data.response[i].rebounds ,
+            'fouls' : data.response[i].fouls ,
+            'points' : data.response[i].points ,
+            'twoPoints' : data.response[i].twoPoints ,
+            'threePoints' : data.response[i].threePoints ,
+            'number' : i
+          };
+          var html = template(context);
+          // INSERISCO LA SCHEDA NELL'ALBUM
+          $('.album_figurine_basket').append(html);
+        }
       },
       'error' : function (richiesta) {
         alert("E' avvenuto un errore. "+errore);
       }
+
     });
-  // }
+
+    $('input').val('')
+
+  })
+
 })
